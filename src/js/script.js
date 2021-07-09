13fa620bd852619a600410a6eec0f9e2bb7e8f48
GGO-web <iicandyoii@gmail.com>
2021-07-09 17:45:33 +0300

    Added Hero section layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,27 @@
-"use strict"
+"use strict";
 
-const da = new DynamicAdapt("max")
-da.init()
+const da = new DynamicAdapt("max");
+da.init();
+
+// parallax.js
+const scene = document.querySelector(".hero__parallax");
+const parallaxInstance = new Parallax(scene);
+
+scene.style = null;
+scene.children.forEach((child) => {
+   child.style = null;
+});
+
+const disableParallax = () => {
+   const disableClass = scene.classList.contains("parallax-disable");
+   if (window.innerWidth < 1024 && !disableClass) {
+      parallaxInstance.disable();
+      scene.classList.add("parallax-disable");
+   } else if (window.innerWidth >= 1024 && disableClass) {
+      parallaxInstance.enable();
+      scene.classList.remove("parallax-disable");
+   }
+};
+disableParallax();
+
+window.addEventListener("resize", disableParallax);
